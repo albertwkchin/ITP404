e186b0d2ef8af307d06d1704d08b4c031e26a490
Albert Chin <chinaw@usc.edu>
2014-10-14 18:18:13 -0700

    Workplace crap

---
+++ Demo 7/instagram-example.php
@@ -0,0 +1,22 @@
+<?php
+
+$instagram_endpoint =
+
+?>
+
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+
+<h1>Cats of Instagram</h1>
+
+<script>
+    var instagram_data = "";
+</script>
+
+
+</body>
+</html>
